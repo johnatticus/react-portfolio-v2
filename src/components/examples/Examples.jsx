1513bd99d7b3d54ba8c657a5055ea716be0e0bd3
John Hysong <johnahysong@gmail.com>
2022-06-06 00:14:26 -0400

    styling for projects etc

--- src/components/examples/Examples.jsx
+++ src/components/examples/Examples.jsx
@@ -5,9 +5,9 @@ const Examples = ({img,link}) => {
   return (
     <div className="p">
       <div className="p-browser">
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
+        <div className="p-circle-r"></div>
+        <div className="p-circle-y"></div>
+        <div className="p-circle-g"></div>
       </div>
       <a href={link} target="_blank" rel="noreferrer">
         <img src={img} alt="" className="p-img" />
